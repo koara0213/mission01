5d1b3f14793db87d27891d1a405b54d881d3638e
koara0213 <mayu.koakoa.pearl@gmail.com>
2025-05-07 23:07:22 +0900

    20250507_slideband_ドラフト板

---
+++ js/slideband.js
@@ -0,0 +1,40 @@
+// 帯のスライドイン //
+const fadeElems = document.querySelectorAll('.fadein');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); // 一度だけ実行
+    }
+  });
+}, {
+  threshold: 0.1 // 10%見えたらトリガー
+});
+
+fadeElems.forEach(el => {
+  observer.observe(el);
+});
+
+// 3カラムを順番に下からスライドイン //
+document.addEventListener('DOMContentLoaded', () => {
+  const boxes = document.querySelectorAll('.imgbox');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        // 順番にアニメーション
+        setTimeout(() => {
+          entry.target.classList.add('slide-in');
+        }, index * 300); // 各要素に300msの遅延
+        observer.unobserve(entry.target); // 一度だけ実行
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  boxes.forEach(box => {
+    observer.observe(box);
+  });
+});
